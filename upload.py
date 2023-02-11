c514017c19d369cf934f5a7f6a3c69d444effbf9
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-02-12 00:57:10 +0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,50 +1,33 @@
+import sys
+import time
 import libtorrent as lt
-import sys, time
-
-
-videoFile = "test.txt"
-workingPath = "."
 
 #Create torrent
-
-f = lt.file_storage()
-lt.add_files(f, videoFile)
-t = lt.create_torrent(f)
-t.add_node("router.utorrent.com", 6881)
-t.add_node("dht.transmissionbt.com", 6881)
-lt.set_piece_hashes(t, workingPath)
-torrent = t.generate()
-f = open("1.torrent", "wb")
+fs = lt.file_storage()
+lt.add_files(fs, "./test.txt")
+t = lt.create_torrent(fs)
+t.add_tracker("udp://tracker.openbittorrent.com:80/announce", 0)
+t.set_creator('libtorrent %s' % lt.version)
+t.set_comment("Test")
+lt.set_piece_hashes(t, ".")
+torrent = t.generate()    
+f = open("mytorrent.torrent", "wb")
 f.write(lt.bencode(torrent))
 f.close()
 
-#Seeding
+#Seed torrent
+ses = lt.session({'listen_interfaces': '0.0.0.0:6881'})
 
-PORT_RANGE = (6881,6891)
-s = lt.session()
-s.listen_on(PORT_RANGE[0],PORT_RANGE[1])
-s.add_dht_router('router.utorrent.com',6881)
-s.start_dht()
-print("DHT start: ", s.is_dht_running())
-print("DHT state: ", s.dht_state())
-
-
-params = {
-            'save_path': workingPath,
-            'storage_mode': lt.storage_mode_t.storage_mode_sparse,
-            'ti': lt.torrent_info(torrent),
-            'seed_mode': True,
-            'paused': False,
-            'upload_mode':True,
-            'super_seeding':True
-        }
-h = s.add_torrent(params)
-print("Total size: " + str(h.status().total_wanted))
-print("Name: " + h.name())
+h = ses.add_torrent({'ti': lt.torrent_info('mytorrent.torrent'), 'save_path': '.', 'seed_mode': True}) 
+print("Total size: ") + str(h.status().total_wanted)
+print("Name: ") + h.name()   
 while True:
     s = h.status()
-    msg = '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s'
-    print(msg % (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, s.state))
+    state_str = ['queued', 'checking', 'downloading metadata', \
+      'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
+
+    print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+      (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
     sys.stdout.flush()
+
     time.sleep(1)
-    exit() 
