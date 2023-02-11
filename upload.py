743d4dd57f84209898e381264072827347bcd543
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-02-12 01:44:02 +0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -35,17 +35,17 @@ h = ses.add_torrent({'ti': info, 'save_path': '.'})
 s = h.status()
 print('starting', s.name)
 
-while (not s.is_seeding):
+while (true):
     s = h.status()
 
     print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
         s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
         s.num_peers, s.state), end=' ')
 
-    alerts = ses.pop_alerts()
-    for a in alerts:
-        if a.category() & lt.alert.category_t.error_notification:
-            print(a)
+#    alerts = ses.pop_alerts()
+#    for a in alerts:
+#        if a.category() & lt.alert.category_t.error_notification:
+#            print(a)
 
     sys.stdout.flush()
 
