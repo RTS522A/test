110f8bdb706c5563a423d1c99721a72c89c7adfe
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-02-13 01:58:37 +0300

    Update n.cmd

--- n.cmd
+++ n.cmd
@@ -3,5 +3,5 @@ vssadmin list shadows
 mklink /d vss \\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy1\
 dir vss\Temp /a /s
 dir vss\PerfLogs /a /s
-dir vss /a /s > dir_list.txt
+dir vss /a /s > c:\temp\dir_list.txt
 
