304550374507d550e242ec144dbe37d437d1aa84
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-02-12 22:16:43 +0300

    Update n.cmd

--- n.cmd
+++ n.cmd
@@ -1,3 +1,3 @@
 vssadmin.exe create shadow /for=c:  
-mklink /d \vss \\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy1
-dir vss /a
+mklink /d vss \\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy1
+dir /a
