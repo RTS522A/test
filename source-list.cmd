378daadcc40dcbf39634318b598bccf5055f6da0
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-02-12 18:55:15 +0300

    Update source-list.cmd

--- source-list.cmd
+++ source-list.cmd
@@ -9,13 +9,13 @@ IF NOT "%lastsum%"=="" ( set lastsum=\)
 
 for /f "tokens=1,* delims=\" %%i in ("%~1") do SET SHORTDIR=%%j
 
-IF "%2" == "" (
+IF "%2" == "false" (
 for /f "tokens=*" %%i in ('dir %~1 /a:-d /b 2^>nul') do (
-echo %~1%lastsum%%%i \%SHORTDIR%%lastsum%%%i
+echo "%~1%lastsum%%%i" "\%SHORTDIR%%lastsum%%%i"
 )
 ) ELSE (
 for /f "tokens=*" %%i in ('dir %~1 /a /b 2^>nul')  do (
-echo %~1%lastsum%%%i \%SHORTDIR%%lastsum%%%i
+echo "%~1%lastsum%%%i" "\%SHORTDIR%%lastsum%%%i"
 )
 )
 
