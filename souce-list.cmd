2c2fa2776235347a3006ac4f737c0107f67ea9bf
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-02-12 17:41:16 +0300

    Create souce-list.cmd

---
+++ souce-list.cmd
@@ -0,0 +1,25 @@
+@echo off
+@chcp 65001>nul
+::echo on
+
+FOR /F "delims=|" %%A IN ("%1") DO (
+    SET lastsum=%%~nxA
+)
+IF NOT "%lastsum%"=="" ( set lastsum=\)
+
+for /f "tokens=2,* delims=\" %%i in ("%~1") do SET SHORTDIR=%%j
+
+IF "%2" == "" (
+for /f "tokens=*" %%i in ('dir %~1 /a:-d /b 2^>nul') do (
+echo %~1%lastsum%%%i \%SHORTDIR%%lastsum%%%i
+)
+) ELSE (
+for /f "tokens=*" %%i in ('dir %~1 /a /b 2^>nul')  do (
+echo %~1%lastsum%%%i \%SHORTDIR%%lastsum%%%i
+)
+)
+
+
+
+
+
